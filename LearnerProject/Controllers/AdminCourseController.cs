fc325be55e8449cab8040140b5759025484b707a
Erhan Gündüz <erhangndz@live.com>
2024-04-16 22:32:00 +0300

    11. ders sonu

---
+++ LearnerProject/Controllers/AdminCourseController.cs
@@ -0,0 +1,78 @@
+﻿using LearnerProject.Models.Context;
+using LearnerProject.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnerProject.Controllers
+{
+    public class AdminCourseController : Controller
+    {
+        LearnerContext context = new LearnerContext();
+        public ActionResult Index()
+        {
+            var values = context.Courses.ToList();
+            return View(values);
+        }
+
+        public ActionResult DeleteCourse(int id)
+        {
+            var value = context.Courses.Find(id);
+            context.Courses.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult AddCourse()
+        {
+            List<SelectListItem> category = (from x in context.Categories.Where(x=>x.Status==true).ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = x.CategoryName,
+                                                 Value = x.CategoryId.ToString()
+                                             }).ToList();
+
+            ViewBag.category = category;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddCourse(Course course)
+        {
+            context.Courses.Add(course);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult UpdateCourse(int id)
+        {
+            List<SelectListItem> category = (from x in context.Categories.Where(x => x.Status == true).ToList()
+                                             select new SelectListItem
+                                             {
+                                                 Text = x.CategoryName,
+                                                 Value = x.CategoryId.ToString()
+                                             }).ToList();
+
+            ViewBag.category = category;
+            var value = context.Courses.Find(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCourse(Course course)
+        {
+            var value = context.Courses.Find(course.CourseId);
+            value.CourseName=course.CourseName;
+            value.CategoryId = course.CategoryId;
+            value.ImageUrl = course.ImageUrl;
+            value.Price = course.Price;
+            value.Description = course.Description;
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ LearnerProject/Controllers/AdminDashboardController.cs
@@ -0,0 +1,25 @@
+﻿using LearnerProject.Models.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LearnerProject.Controllers
+{
+    public class AdminDashboardController : Controller
+    {
+        LearnerContext context = new LearnerContext();
+        public ActionResult Index()
+        {
+            ViewBag.v1 = context.Courses.Count();
+            ViewBag.v2 = context.Categories.Count();
+            ViewBag.v3 = context.Classrooms.Count();
+            ViewBag.v4 = context.Students.Count();
+            ViewBag.v5 = context.Courses.OrderByDescending(x => x.Price).Select(x => x.CourseName).FirstOrDefault();
+            ViewBag.v6 = context.Courses.Where(x => x.Category.CategoryName == "Kodlama").Count();
+            ViewBag.v7 = context.Reviews.OrderByDescending(x=>x.ReviewValue).Select(x=>x.Course.CourseName).FirstOrDefault();
+            return View();
+        }
+    }
+}
